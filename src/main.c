8835ab3a36af8f67b228acbbe3fe69d0e8dd7c86
isaac.mccracken@gmail.com <isaac.mccracken@gmail.com>
2023-01-08 13:53:22 -0700

    okay that took a long time

--- src/main.c
+++ src/main.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
 
-int main() {
+char* open_file(const char* path);
 
+int main(int argc, const char** argv) {
+    char* source = NULL;
+    if (argc != 0) {
+        source = open_file(argv[1]);
+        printf("%s\n", source);
+    } else {
+        fprintf(stderr, "No input files: bro :(\n");
+    }
+}
+
+char* open_file(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) 
+    {
+        fprintf(stderr, "Could not open file \"%s\"\n", path);
+        exit(74);
+    }
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* text_buffer = (char*)malloc(fileSize + 1);
+    if (text_buffer == NULL) 
+    {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    size_t bytesRead = fread(text_buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize)
+    {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+
+    text_buffer[bytesRead] = '\0';
+    printf("BYTES READ: %zu\n", bytesRead);
+    fclose(file);
+    return text_buffer;
 }
---
+++ src/types.h
@@ -0,0 +1,20 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdbool.h>
+
+typedef unsigned char U8;
+typedef unsigned short U16;
+typedef unsigned long U32;
+typedef unsigned long long U64;
+
+typedef signed char I8;
+typedef signed short I16;
+typedef signed long I32;
+typedef signed long long I64;
+
+typedef float F32;
+typedef double F64;
+
+
+#endif // TYPES_H
